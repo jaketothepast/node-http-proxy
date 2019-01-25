c7c1bb53920678f643c15ccc8e0e40c10553ec8b
jaketothepast <windlejacob12@gmail.com>
2019-01-24 22:07:42 -0500

    Starting HTTP parsing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node HTTP Proxy
+
+This package is an HTTP proxy, written in Node.js. Spun up on a virtual machine.
--- index.js
+++ index.js
@@ -1,4 +1,41 @@
 var net = require('net');
+var http = require('http');
+
+class HttpRequest {
+    constructor({...attrs}) {
+        this.type = attrs.type;
+        this.headers = attrs.headers;
+        this.body = attrs.body;
+    }
+}
+
+/**
+ * Parse an HTTP request.
+ * @param {String} bytes 
+ */
+function parseHttpRequest(bytes) {
+    let httpRequestAttrs = {};
+
+    bytes.split("\r\n").map((row) => {
+        // Match against HTTP verbs from the first row.
+        if (row.match('GET|POST|PUT|PATCH|UPDATE|DELETE')) {
+            console.log("FIRST ROW");
+            console.log(row);
+            let typeInfo = row.split(" ");
+
+            if (typeInfo.length !== 3) {
+                console.log("Bad HTTP request");
+                return null;
+            }
+
+            httpRequestAttrs["type"] = typeInfo[0];
+            httpRequestAttrs["path"] = typeInfo[1];
+            httpRequestAttrs["httpVersion"] = typeInfo[2];
+        } else {
+            console.log(row);
+        }
+    })
+}
 
 /**
  * Log the request and forward it.
@@ -13,7 +50,7 @@ var server = net.createServer((socket) => {
         console.log("Client disconnected");
     });
     socket.on("data", (c) => {
-        logAndSend(c.toString());
+        parseHttpRequest(c.toString());
     });
 })
 
