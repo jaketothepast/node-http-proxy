ea45d63909a05332dfedf5c5ae03cc3e6c011aba
jaketothepast <windlejacob12@gmail.com>
2019-01-24 20:43:49 -0500

    Got socket.on working

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var net = require('net');
 
+/**
+ * Log the request and forward it.
+ * @param {String} bytes 
+ */
 async function logAndSend(bytes) {
-
+    console.log(bytes)
 }
 
 var server = net.createServer((socket) => {
     socket.on('end', (c) => {
         console.log("Client disconnected");
     });
+    socket.on("data", (c) => {
+        logAndSend(c.toString());
+    });
 })
 
-server.on('connection', () => {
-    console.log("Client connected!");
+server.on('connection', (c) => {
+    console.log("Client connected!" + c.read(10));
 });
+
+server.on("listening", (c) => {
+    console.log("LISTENING");
+})
 
 server.listen(8124, () => {
     console.log("Server bound.");
