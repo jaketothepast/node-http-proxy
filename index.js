f8aa81be8de1515e5f85f20afed5a79ab361a226
jaketothepast <windlejacob12@gmail.com>
2019-01-27 21:29:10 -0500

    Implement blocking functionality after certain count of visits, added tests

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const logger = winston.createLogger({
 /**
  * Connection handler for proxy, forward request and then pipe response
  * back to the client.
- * 
+ *
  * @param {IncomingMessage} request The request from the client
  * @param {OutgoingMessage} response The response to write back to the client
  */
@@ -28,23 +28,48 @@ const processRequest = (request, response) => {
     // Set up a data handler for the socket connection.
     request.on("data", (chunk) => {
         body.push(chunk);
-    })
-    
+    });
+
     // Set up an end handler for the socket connection.
     request.on("end", () => {
         body = Buffer.concat(body).toString();
         logger.info({message: "Requesting: " + req.url});
 
         const hostUrl = new url.URL(req.url);
-        myDb.visitHost(hostUrl.hostname);
-        // Complicated line of JS to forward request, and pipe it to the
-        // response object.
-        //
-        // Uses closures to ensure that both req and res are what I want.
-        req.pipe(http.request(req.url, (resp) => {resp.pipe(res)}));
+
+        // Do the work of actually updating our db with the visit.
+        // Val in the callback will be false if blocked.
+        myDb.visitHost(hostUrl.hostname, (val) => {
+            if (val === false) {
+                // blocked.
+                console.log("Blocked you fool.")
+                sendBlockPage(res);
+            } else {
+                // Not blocked
+                req.pipe(http.request(req.url, (resp) => {
+                    resp.pipe(res);
+                }))
+            }
+        });
     });
 }
 
+/**
+ * Send a block page back to the client.
+ * @param {OutgoingMessage} res Response to the client
+ */
+function sendBlockPage(res) {
+    res.writeHead(403, {
+        'Content-Type': 'text/html',
+        'X-Powered-By': 'bacon'
+    })
+    res.write('<html><body><h1>BLOCKED BY MY PROXY</h1></body></html>');
+    res.end();
+}
+
+/**
+ * Start the server.
+ */
 function startServer() {
     // Start the server, set up data and end handlers.
     var server = http.createServer(processRequest);
--- test/test_proxy.py
+++ test/test_proxy.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 # TODO - Make this configurable based on config file test section.
 http_proxy = "http://127.0.0.1:8124"
@@ -7,4 +8,8 @@ def test_proxy_basic():
     resp = requests.get("http://httpbin.org", proxies={"http": http_proxy})
     assert resp.status_code == 200
 
+def test_proxy_block_page():
+    for i in range(2):
+        requests.get("http://httpbin.org", proxies={"http": http_proxy})
+    assert requests.get("http://httpbin.org", proxies={"http": http_proxy}).status_code == 403
     
