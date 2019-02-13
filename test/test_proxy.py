5a40a15f9c1e7d891448d9a8c7af433d90a12086
jaketothepast <windlejacob12@gmail.com>
2019-02-13 16:34:56 -0500

    Added ability to add host to blocked table if visit count is exceeded

--- test/test_proxy.py
+++ test/test_proxy.py
@@ -13,7 +13,15 @@ def test_proxy_block_page():
         requests.get("http://httpbin.org", proxies={"http": http_proxy})
     assert requests.get("http://httpbin.org", proxies={"http": http_proxy}).status_code == 403
 
+    # TODO - Use a sqlite driver here to check that httpbin.org is in the blocked table.
+    #        Or mock the sqlite client.
+
 def test_blocked_file():
+    """
+    Tests that the host configured to be blocked is successfully blocked.
+
+    TODO - Mock the blocked file to be used by the proxy to not have to hardcode the host.
+    """
     resp = requests.get("http://wikipedia.org", proxies={"http": http_proxy})
     assert resp.status_code == 403
     
